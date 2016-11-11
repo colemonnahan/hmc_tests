065beee4fa721e1e67fffdf4f3f12a0a6d434f2c
colemonnahan <monnahc@uw.edu>
2016-11-11 14:25:04 -0800

    Update mvnd1

--- models/mvnd1/mvnd_tmb.cpp
+++ models/mvnd1/mvnd_tmb.cpp
@@ -4,11 +4,11 @@ template<class Type>
 Type objective_function<Type>::operator() ()
 {
   DATA_INTEGER(Npar); // dimension of matrix
-  DATA_MATRIX(covar);
-  PARAMETER_VECTOR(mu); // parameters
+  DATA_SCALAR(covar);
+  PARAMETER(mu); // parameters
   using namespace density;
-  MVNORM_t<Type> neg_log_density(covar);
-  Type nll= neg_log_density(mu);
-  REPORT(nll);
+  // MVNORM_t<Type> neg_log_density(covar);
+  // Type nll= neg_log_density(mu);
+  Type nll= -dnorm(mu, Type(0), covar, true);
   return(nll);
 }
