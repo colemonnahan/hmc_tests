e99b79636bb942d8dded73b17e5f1a9ac3003e54
colemonnahan <monnahc@uw.edu>
2017-01-30 18:51:23 -0800

    Add code to test admb step size adaptation

--- adapt_tests.R
+++ adapt_tests.R
@@ -11,12 +11,11 @@ devtools::load_all("c:/Users/Cole/adnuts")
 ## and ADMB Start with simple MVN model.
 set.seed(235)
 Npar <- 2
-corr <- matrix(-.954, nrow=2, ncol=2)
+corr <- matrix(.98, nrow=2, ncol=2)
 diag(corr) <- 1
-se <- c(1, 5)
+se <- c(1, .10)
 covar <- corr * (se %o% se)
-covar2 <- diag(2); diag(covar2) <- se^2
-covar2[1,2] <- covar2[2,1] <- 4
+covar2 <- diag(x=se^2)
 covar.inv <- solve(covar)
 samples <- mvtnorm::rmvnorm(n=1e5, sigma=covar)
 apply(samples, 2, var)
@@ -26,23 +25,45 @@ compile(file='models/mvnd/mvnd_tmb.cpp')
 dyn.load(dynlib('models/mvnd/mvnd_tmb'))
 data <- list(covar=covar, Npar=Npar, x=rep(0, len=Npar))
 mvnd.obj <- MakeADFun(data=data, parameters=list(mu=c(0,0)), DLL='mvnd_tmb')
+model.path <- 'models/mvnd'
+model.name <- 'mvnd'
+write.table(x=c(2, covar), file='models/mvnd/mvnd.dat', row.names=FALSE, col.names=FALSE)
+system('models/mvnd/mvnd')
 
-chains <- 5
-init <- lapply(1:chains, function(x) rnorm(2, sd=se*10))
-iter <- 2000
-tmb1 <- run_mcmc(mvnd.obj, iter=iter, init=init, chains=chains, covar=NULL)
+chains <- 15
+init <- lapply(1:chains, function(x) rnorm(2, sd=se*1))
+iter <- 1000
+tmb1 <- run_mcmc(mvnd.obj, iter=iter, init=init, chains=chains, covar=diag(2))
 tmb2 <- run_mcmc(mvnd.obj, iter=iter, init=init, chains=chains, covar=covar2)
 tmb3 <- run_mcmc(mvnd.obj, iter=iter, init=init, chains=chains, covar=covar)
-eps.tmb1 <- laply(tmb1$sampler_params, function(x) tail(x[,2],1))
-eps.tmb2 <- laply(tmb2$sampler_params, function(x) tail(x[,2],1))
-eps.tmb3 <- laply(tmb3$sampler_params, function(x) tail(x[,2],1))
-boxplot(eps.tmb1, eps.tmb2, eps.tmb3)
+adapt.tmb1 <- cbind(form=1, extract_adapt(tmb1))
+adapt.tmb2 <- cbind(form=2, extract_adapt(tmb2))
+adapt.tmb3 <- cbind(form=3, extract_adapt(tmb3))
+adapt.tmb <- rbind(adapt.tmb1, adapt.tmb2, adapt.tmb3)
+admb1 <- run_admb_mcmc(model.path, model.name, iter=iter, init=init, chains=chains, covar=diag(2))
+admb2 <- run_admb_mcmc(model.path, model.name, iter=iter, init=init, chains=chains, covar=covar2)
+admb3 <- run_admb_mcmc(model.path, model.name, iter=iter, init=init, chains=chains, covar=covar)
+adapt.admb1 <- cbind(form=1, extract_adapt(admb1))
+adapt.admb2 <- cbind(form=2, extract_adapt(admb2))
+adapt.admb3 <- cbind(form=3, extract_adapt(admb3))
+adapt.admb <- rbind(adapt.admb1, adapt.admb2, adapt.admb3)
+
+adapt <- rbind(cbind(name='tmb', adapt.tmb),
+               cbind(name='admb', adapt.admb))
+ggplot(adapt, aes(form, stepsize, color=name)) + geom_jitter()
+ggplot(adapt, aes(form, nsteps.median, color=name)) + geom_jitter()
 ## launch_shinystan_tmb(tmb1)
 ## launch_shinystan_tmb(tmb2)
 ## launch_shinystan_tmb(tmb3)
 
-
-
+extract_adapt <- function(fit){
+  ldply(1:length(fit$sampler_params), function(i){
+    ind <- -(1:fit$warmup)
+    xx <- fit$sampler_params[[i]]
+    cbind(chain=i, stepsize=tail(xx[ind,2],1), nsteps.median= median(xx[ind,4]),
+          nsteps.mean=mean(xx[ind,4]),
+          accept.prob=mean(xx[ind,1]))})
+}
 
 
 
