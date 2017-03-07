1a83bbdf7dbad81dad825b605996b1279409c03f
colemonnahan <monnahc@uw.edu>
2017-03-07 13:49:53 -0800

    Ignore more admb temp files

---
+++ models/mvnd/admb/mvnd.cpp
@@ -0,0 +1,115 @@
+#ifdef DEBUG
+  #ifndef __SUNPRO_C
+    #include <cfenv>
+    #include <cstdlib>
+  #endif
+#endif
+  #include <mvnorm.h>
+  MVNORM_t neg_log_density;
+#include <admodel.h>
+#include <contrib.h>
+
+  extern "C"  {
+    void ad_boundf(int i);
+  }
+#include <mvnd.htp>
+
+model_data::model_data(int argc,char * argv[]) : ad_comm(argc,argv)
+{
+  dim.allocate("dim");
+  covar.allocate(1,dim,1,dim,"covar");
+}
+
+model_parameters::model_parameters(int sz,int argc,char * argv[]) : 
+ model_data(argc,argv) , function_minimizer(sz)
+{
+  initializationfunction();
+  mu.allocate(1,dim,"mu");
+  jnll.allocate("jnll");
+  prior_function_value.allocate("prior_function_value");
+  likelihood_function_value.allocate("likelihood_function_value");
+}
+
+void model_parameters::preliminary_calculations(void)
+{
+
+#if defined(USE_ADPVM)
+
+  admaster_slave_variable_interface(*this);
+
+#endif
+  neg_log_density.setSigma(covar);
+}
+
+void model_parameters::userfunction(void)
+{
+  jnll =0.0;
+  dvector x(1,dim);
+  for(int i=1; i<=dim; i++) x(i)=0;
+  jnll=neg_log_density(x-mu);
+}
+
+model_data::~model_data()
+{}
+
+model_parameters::~model_parameters()
+{}
+
+void model_parameters::report(const dvector& gradients){}
+
+void model_parameters::final_calcs(void){}
+
+void model_parameters::set_runtime(void){}
+
+#ifdef _BORLANDC_
+  extern unsigned _stklen=10000U;
+#endif
+
+
+#ifdef __ZTC__
+  extern unsigned int _stack=10000U;
+#endif
+
+  long int arrmblsize=0;
+
+int main(int argc,char * argv[])
+{
+    ad_set_new_handler();
+  ad_exit=&ad_boundf;
+  arrmblsize=20000000;
+    gradient_structure::set_NO_DERIVATIVES();
+#ifdef DEBUG
+  #ifndef __SUNPRO_C
+std::feclearexcept(FE_ALL_EXCEPT);
+  #endif
+#endif
+    gradient_structure::set_YES_SAVE_VARIABLES_VALUES();
+    if (!arrmblsize) arrmblsize=15000000;
+    model_parameters mp(arrmblsize,argc,argv);
+    mp.iprint=10;
+    mp.preliminary_calculations();
+    mp.computations(argc,argv);
+#ifdef DEBUG
+  #ifndef __SUNPRO_C
+bool failedtest = false;
+if (std::fetestexcept(FE_DIVBYZERO))
+  { cerr << "Error: Detected division by zero." << endl; failedtest = true; }
+if (std::fetestexcept(FE_INVALID))
+  { cerr << "Error: Detected invalid argument." << endl; failedtest = true; }
+if (std::fetestexcept(FE_OVERFLOW))
+  { cerr << "Error: Detected overflow." << endl; failedtest = true; }
+if (std::fetestexcept(FE_UNDERFLOW))
+  { cerr << "Error: Detected underflow." << endl; }
+if (failedtest) { std::abort(); } 
+  #endif
+#endif
+    return 0;
+}
+
+extern "C"  {
+  void ad_boundf(int i)
+  {
+    /* so we can stop here */
+    exit(i);
+  }
+}
