7c2e1cddb40fb1978e770507137e9bbe1e6bbde6
colemonnahan <monnahc@uw.edu>
2017-03-13 19:16:39 -0700

    Tweaks to run new ss3sim model, almost ready

--- ss3_models/run_cod_fast.R
+++ ss3_models/run_cod_fast.R
@@ -1,6 +1,6 @@
 library(adnuts)
 library(snowfall)
-
+library(shinystan)
 
 setwd('cod_fast')
 mle <- r4ss::read.admbFit('ss3')
@@ -18,3 +18,10 @@ fit.nuts <-
               parallel=TRUE,
   chains=cc, warmup=2, dir='cod_fast', cores=3,
   control=list(max_treedepth=8, stepsize=.1))
+launch_shinystan_admb(fit.nuts)
+
+fit.nuts <-
+  sample_admb('ss3', iter=50, init=inits, par.names=par.names,
+              parallel=TRUE,
+  chains=cc, warmup=2, dir='cod_fast', cores=3,
+  control=list(algorithm='RWM'))
