6647e0b34f65c1646c3d48a3139aeffecfe1fb80
colemonnahan <monnahc@uw.edu>
2017-03-18 19:02:58 -0700

    Clean up cod run code

--- ss3_models/run_cod_fast.R
+++ ss3_models/run_cod_fast.R
@@ -1,27 +1,35 @@
+setwd("ss3_models/")
 library(adnuts)
 library(snowfall)
 library(shinystan)
 
-setwd('cod_fast')
+m <- 'cod_fast'
+setwd(m)
 mle <- r4ss::read.admbFit('ss3')
 par.names <- mle$names[1:mle$nopar]
 setwd('..')
-
 sfStop()
-sfInit(parallel=TRUE, cpus=3)
-cc <- 3
-inits <- rep(list(mle$est[1:mle$nopar]), cc)
+reps <- 3
+sfInit(parallel=TRUE, cpus=reps)
+inits <- rep(list(mle$est[1:mle$nopar]), reps)
+td <- 8
+warmup <- 25
+iter <- 225
 sfExportAll()
 
 fit.nuts <-
-  sample_admb('ss3', iter=50, init=inits, par.names=par.names,
-              parallel=TRUE,
-  chains=cc, warmup=2, dir='cod_fast', cores=3,
-  control=list(max_treedepth=8, stepsize=.1))
+  sample_admb('ss3', iter=iter, init=inits, par.names=par.names,
+              parallel=TRUE, chains=reps, warmup=warmup, dir=m, cores=reps,
+              control=list(max_treedepth=td, stepsize=.3))
+stats.nuts <- data.frame(rstan::monitor(fit.nuts$samples, warmup=warmup, probs=.5, print=FALSE))
+perf.nuts <- min(stats.nuts$n_eff)/sum(fit.nuts$time.total)
 launch_shinystan_admb(fit.nuts)
 
-fit.nuts <-
-  sample_admb('ss3', iter=50, init=inits, par.names=par.names,
-              parallel=TRUE,
-  chains=cc, warmup=2, dir='cod_fast', cores=3,
-  control=list(algorithm='RWM'))
+
+fit.rwm <-
+  sample_admb('ss3', iter=iter, init=inits, par.names=par.names,
+              parallel=TRUE, chains=reps, warmup=warmup, dir=m, cores=reps,
+              control=list(algorithm='RWM'))
+launch_shinystan_admb(fit.rwm)
+stats.rwm <- data.frame(rstan::monitor(fit.rwm$samples, warmup=warmup, probs=.5, print=FALSE))
+perf.rwm <- min(stats.rwm$n_eff)/sum(fit.rwm$time.total)
--- ss3_models/run_ss3_models.R
+++ ss3_models/run_ss3_models.R
@@ -1,22 +1,22 @@
-
+setwd('ss3_models/')
 devtools::install('C:/Users/Cole/adnuts')
 library(shinystan)
 library(adnuts)
 library(snowfall)
-cores <- 4
+cores <- 3
 sfStop()
 sfInit(parallel=TRUE, cpus=cores)
 chains <- cores
 inits <- rep(list('mle'), chains)
-m <- 'cod_rich'
-mle <- r4ss::read.admbFit('cod_rich/ss3')
+m <- 'cod_fast'
+mle <- r4ss::read.admbFit('cod_fast/ss3')
 inits <- rep(list(mle$est[1:mle$nopar]), chains)
 par.names <- mle$names[1:mle$nopar]
 sfExportAll()
 
-codrich <- sample_admb(model='ss3', chains=chains, dir=m, iter=10,
+cod <- sample_admb(model='ss3', chains=chains, dir=m, iter=10,
                        init=inits, par.names=par.names, warmup=2,
-                       parallel=F, cores=cores,
+                       parallel=TRUE, cores=cores,
                        control=list(stepsize=.2, max_treedepth=3))
 
 launch_shinystan_admb(codrich)
