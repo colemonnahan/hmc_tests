9e49e6893e9936ee8b85f1fb59077f06f579a46b
colemonnahan <monnahc@uw.edu>
2017-03-18 21:15:34 -0700

    Try some longer runs for nuts vs rwm

--- ss3_models/run_cod_fast.R
+++ ss3_models/run_cod_fast.R
@@ -9,27 +9,35 @@ mle <- r4ss::read.admbFit('ss3')
 par.names <- mle$names[1:mle$nopar]
 setwd('..')
 sfStop()
-reps <- 3
+reps <- 5
 sfInit(parallel=TRUE, cpus=reps)
 inits <- rep(list(mle$est[1:mle$nopar]), reps)
-td <- 8
-warmup <- 25
-iter <- 225
+td <- 12
+warmup <- 50
+iter <- 1000
+hh <- .1                           # hours to run
+eps <- .1
+mm <- diag(length(par.names))
 sfExportAll()
 
 fit.nuts <-
   sample_admb('ss3', iter=iter, init=inits, par.names=par.names,
-              parallel=TRUE, chains=reps, warmup=warmup, dir=m, cores=reps,
-              control=list(max_treedepth=td, stepsize=.3))
+              duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup, dir=m, cores=reps,
+              control=list(max_treedepth=td, stepsize=eps, metric=mm))
+saveRDS(fit.nuts, file='fit.nuts.RDS')
 stats.nuts <- data.frame(rstan::monitor(fit.nuts$samples, warmup=warmup, probs=.5, print=FALSE))
 perf.nuts <- min(stats.nuts$n_eff)/sum(fit.nuts$time.total)
-launch_shinystan_admb(fit.nuts)
-
-
+tt <- 1000 # thin rate
 fit.rwm <-
-  sample_admb('ss3', iter=iter, init=inits, par.names=par.names,
-              parallel=TRUE, chains=reps, warmup=warmup, dir=m, cores=reps,
-              control=list(algorithm='RWM'))
-launch_shinystan_admb(fit.rwm)
+  sample_admb('ss3', iter=iter*tt, duration=hh*60, init=inits, par.names=par.names,
+              parallel=TRUE, chains=reps, warmup=warmup*tt, dir=m, cores=reps,
+              thin=tt, control=list(algorithm='RWM'))
+saveRDS(fit.rwm, file='fit.rwm.RDS')
 stats.rwm <- data.frame(rstan::monitor(fit.rwm$samples, warmup=warmup, probs=.5, print=FALSE))
 perf.rwm <- min(stats.rwm$n_eff)/sum(fit.rwm$time.total)
+
+## How much faster is NUTS?
+perf.nuts/perf.rwm
+
+launch_shinystan_admb(fit.nuts)
+launch_shinystan_admb(fit.rwm)
