8e62ead88239d33a3b992243b74b62a2c2ea9741
colemonnahan <monnahc@uw.edu>
2017-03-19 17:37:44 -0700

    Quick changes to try cod_fast

--- ss3_models/run_cod_fast.R
+++ ss3_models/run_cod_fast.R
@@ -6,10 +6,10 @@ library(shinystan)
 ## Run model if needed
 m <- 'cod_fast'
 setwd(m)
-## system("ss3")
-## replist <- r4ss::SS_output(getwd(), covar=TRUE)
-## unlink('plots', TRUE)
-## r4ss::SS_plots(replist)
+system("ss3")
+replist <- r4ss::SS_output(getwd(), covar=TRUE)
+unlink('plots', TRUE)
+r4ss::SS_plots(replist)
 setwd('..')
 
 
@@ -18,14 +18,14 @@ mle <- r4ss::read.admbFit('cod_fast/ss3')
 covar <- get.admb.cov(m)$cov.bounded
 N <- mle$nopar
 par.names <- mle$names[1:N]
-reps <- 3                        # chains/reps to run
+reps <- 6                        # chains/reps to run
 ## Draw inits from MVN using MLE and covar
 inits <- rep(list(as.vector(mvtnorm::rmvnorm(n=1, mean=mle$est[1:N], sigma=covar))),reps)
 inits <- rep(list(mle$est[1:N]), reps)
 td <- 12
 warmup <- 1
-iter <- 150
-hh <- .5                           # hours to run
+iter <- 1e6
+hh <- 1                           # hours to run
 eps <- NULL
 mm <- diag(length(par.names))
 sfInit(parallel=TRUE, cpus=reps)
