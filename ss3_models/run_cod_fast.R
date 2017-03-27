875fee77e4713cb3824c9cffec1c0baa2fed505e
colemonnahan <monnahc@uw.edu>
2017-03-26 20:56:59 -0700

    Tests for correct initial values

--- ss3_models/run_cod_fast.R
+++ ss3_models/run_cod_fast.R
@@ -5,13 +5,13 @@ m <- 'cod_fast'
 
 ## ## Run model if needed
 ## setwd(m)
-## ## system('admb ss3')
+## system('admb ss3')
 ## system("ss3")
 ## replist <- r4ss::SS_output(getwd(), covar=TRUE)
 ## unlink('plots', TRUE)
 ## r4ss::SS_plots(replist)
 ## setwd('..')
-
+## temp <- get.admb.cov(m)
 
 sfStop()
 mle <- r4ss::read.admbFit('cod_fast/ss3')
@@ -19,12 +19,12 @@ covar <- get.admb.cov(m)$cov.unbounded
 N <- mle$nopar
 par.names <- mle$names[1:N]
 par.names <- paste0(1:N, "_", par.names)
-reps <- 6                        # chains/reps to run
+reps <- 1                        # chains/reps to run
 ## Draw inits from MVN using MLE and covar
 #inits <- rep(list(as.vector(mvtnorm::rmvnorm(n=1, mean=mle$est[1:N], sigma=covar))),reps)
 inits <- rep(list(mle$est[1:N]), reps)
 td <- 2
-iter <- 100
+iter <- 10
 warmup <- 1
 tt <- 1 # thin rate
 hh <- 10                           # hours to run
@@ -35,16 +35,15 @@ sfExportAll()
 
 fit.nuts <-
   sample_admb('ss3', iter=iter, init=inits, par.names=par.names,
-              duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup, dir=m, cores=reps,
+              duration=hh*60, parallel=FALSE, chains=reps, warmup=warmup, dir=m, cores=reps,
               control=list(max_treedepth=td, stepsize=eps, metric=mm))
 saveRDS(fit.nuts, file='fit.nuts.RDS')
 stats.nuts <- data.frame(rstan::monitor(fit.nuts$samples, warmup=warmup, probs=.5, print=FALSE))
 perf.nuts <- min(stats.nuts$n_eff)/sum(fit.nuts$time.total)
 fit.rwm <-
   sample_admb('ss3', iter=iter*tt, duration=hh*60, init=inits, par.names=par.names,
-              parallel=TRUE, chains=reps, warmup=warmup*tt, dir=m, cores=reps,
-              thin=tt, control=list(algorithm='RWM', metric=mm),
-  admb.args=' -mcmult 100000')
+              parallel=FALSE, chains=reps, warmup=warmup*tt, dir=m, cores=reps,
+              thin=tt, control=list(algorithm='RWM', metric=mm))
 fit.rwm$warmup <- fit.rwm$warmup/tt
 saveRDS(fit.rwm, file='fit.rwm.RDS')
 stats.rwm <- data.frame(rstan::monitor(fit.rwm$samples, warmup=warmup/tt, probs=.5, print=FALSE))
@@ -57,10 +56,8 @@ launch_shinystan_admb(fit.nuts)
 launch_shinystan_admb(fit.rwm)
 
 fit.rwm$samples[1, 1, N+1]
-fit.rwm$samples[100, 1, N+1]
 fit.nuts$samples[1, 1, N+1]
 fit.rwm$samples[1, 1, N]
-fit.rwm$samples[100, 1, N]
 fit.nuts$samples[1, 1, N]
 
 inits=rep(list(fit.rwm$samples[100,1,1:N]+.1),reps)
