fa9f90ec38131d4d60ad5135fb149e66013aaccc
colemonnahan <monnahc@uw.edu>
2017-04-06 10:43:39 -0700

    Quick updates to get long models working

--- admb_models/run_long_models.R
+++ admb_models/run_long_models.R
@@ -1,5 +1,4 @@
 ## Run long chains for individual models using RWM.
-setwd('admb_models/')
 devtools::install('C:/Users/Cole/adnuts')
 library(shinystan)
 library(adnuts)
@@ -10,7 +9,7 @@ hh <- 24 # hours to run
 
 sfStop()
 d <- m <- 'cod_fast'
-thin <- 100
+thin <- 1
 iter <- 2000*thin
 warmup <- 1000*thin
 mle <- r4ss::read.admbFit(paste0(d,'/',m))
@@ -18,12 +17,33 @@ N <- mle$nopar
 par.names <- paste0(1:N, "_", mle$names[1:N])
 ## Draw inits from MVT using MLE and covar
 covar <- get.admb.cov(d)$cov.unbounded
-inits <- lapply(1:reps, function(i) mle$est[1:N]+as.vector(mvtnorm::rmvt(n=1, df=5, sigma=covar)))
+inits <- lapply(1:reps, function(i) mle$est[1:N]+as.vector(mvtnorm::rmvt(n=1, df=50, sigma=covar)))
 sfInit(parallel=TRUE, cpus=reps)
 sfExportAll()
-
 fit.long <-
   sample_admb(m, iter=iter, init=inits, par.names=par.names, thin=thin,
               duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup,
               dir=d, cores=reps, algorithm='RWM')
-launch_shinyadmb(fit.long)
+saveRDS(fit.long, file=paste0("results/long_run_", m, ".RDS")
+## launch_shinyadmb(fit.long)
+
+
+sfStop()
+d <- m <- 'hake'
+thin <- 1
+iter <- 2000*thin
+warmup <- 1000*thin
+mle <- r4ss::read.admbFit(paste0(d,'/',m))
+N <- mle$nopar
+par.names <- paste0(1:N, "_", mle$names[1:N])
+## Draw inits from MVT using MLE and covar
+covar <- get.admb.cov(d)$cov.unbounded
+inits <- lapply(1:reps, function(i) mle$est[1:N]+as.vector(mvtnorm::rmvt(n=1, df=50, sigma=covar)))
+sfInit(parallel=TRUE, cpus=reps)
+sfExportAll()
+fit.hake <-
+  sample_admb(m, iter=iter, init=inits, par.names=par.names, thin=thin,
+              duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup,
+              dir=d, cores=reps, algorithm='RWM')
+saveRDS(fit.hake, file=paste0("results/long_run_", m, ".RDS")
+ launch_shinyadmb(fit.hake)
