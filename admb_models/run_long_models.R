3ee3fc9731ab992156386dcc73e6c51dc6a2cae6
colemonnahan <monnahc@uw.edu>
2017-04-07 16:23:54 -0700

    Add tanner model

--- admb_models/run_long_models.R
+++ admb_models/run_long_models.R
@@ -8,25 +8,28 @@ reps <- 6 # chains to run in parallel
 hh <- 24 # hours to run
 
 sfStop()
+d <- 'snowcrab'; m <- '2016sc'
 d <- m <- 'cod_fast'
-thin <- 100
-iter <- 5000
+thin <- 1
+iter <- 10
 warmup <- iter/2
 mle <- r4ss::read.admbFit(paste0(d,'/',m))
 N <- mle$nopar
 par.names <- paste0(1:N, "_", mle$names[1:N])
 ## Draw inits from MVT using MLE and covar
-covar <- get.admb.cov(d)$cov.unbounded
+covar <- get.admb.cov(d)$cov.bounded
 inits <- lapply(1:reps, function(i) mle$est[1:N]+as.vector(mvtnorm::rmvt(n=1, df=50, sigma=covar)))
 sfInit(parallel=TRUE, cpus=reps)
 sfExportAll()
 fit.rwm <- sample_admb(m, iter=iter*thin, init=inits, par.names=par.names, thin=thin,
               duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup*thin,
-              dir=d, cores=reps, algorithm='RWM')
+              dir=d, cores=reps, algorithm='RWM', extra.args=' -ainp cod_fast.par')
+
 saveRDS(fit.rwm, file=paste0("results/long_rwm_", m, ".RDS"))
 fit.nuts <- sample_admb(m, iter=iter, init=inits, par.names=par.names, thin=1,
-              duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup,
-              dir=d, cores=reps, algorithm='NUTS', control=list(adapt_delta=.9))
+              duration=hh*60, parallel=TRUE, chains=3, warmup=warmup,
+              dir=d, cores=reps, algorithm='NUTS',
+              control=list(adapt_delta=.9))
 saveRDS(fit.nuts, file=paste0("results/long_nuts_", m, ".RDS"))
 
 
