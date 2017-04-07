46a24674dfb35a36ce13296d0804a958fa497296
colemonnahan <monnahc@uw.edu>
2017-04-07 10:43:20 -0700

    Setup to run long chains

--- admb_models/run_long_models.R
+++ admb_models/run_long_models.R
@@ -9,9 +9,9 @@ hh <- 24 # hours to run
 
 sfStop()
 d <- m <- 'cod_fast'
-thin <- 1
-iter <- 2000*thin
-warmup <- 1000*thin
+thin <- 100
+iter <- 5000
+warmup <- iter/2
 mle <- r4ss::read.admbFit(paste0(d,'/',m))
 N <- mle$nopar
 par.names <- paste0(1:N, "_", mle$names[1:N])
@@ -20,19 +20,22 @@ covar <- get.admb.cov(d)$cov.unbounded
 inits <- lapply(1:reps, function(i) mle$est[1:N]+as.vector(mvtnorm::rmvt(n=1, df=50, sigma=covar)))
 sfInit(parallel=TRUE, cpus=reps)
 sfExportAll()
-fit.long <-
-  sample_admb(m, iter=iter, init=inits, par.names=par.names, thin=thin,
-              duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup,
+fit.rwm <- sample_admb(m, iter=iter*thin, init=inits, par.names=par.names, thin=thin,
+              duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup*thin,
               dir=d, cores=reps, algorithm='RWM')
-saveRDS(fit.long, file=paste0("results/long_run_", m, ".RDS")
-## launch_shinyadmb(fit.long)
+saveRDS(fit.rwm, file=paste0("results/long_rwm_", m, ".RDS"))
+fit.nuts <- sample_admb(m, iter=iter, init=inits, par.names=par.names, thin=1,
+              duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup,
+              dir=d, cores=reps, algorithm='NUTS', control=list(adapt_delta=.9))
+saveRDS(fit.nuts, file=paste0("results/long_nuts_", m, ".RDS"))
+
 
 
 sfStop()
 d <- m <- 'hake'
-thin <- 1
-iter <- 2000*thin
-warmup <- 1000*thin
+thin <- 1000
+iter <- 5000
+warmup <- iter/2
 mle <- r4ss::read.admbFit(paste0(d,'/',m))
 N <- mle$nopar
 par.names <- paste0(1:N, "_", mle$names[1:N])
@@ -41,9 +44,12 @@ covar <- get.admb.cov(d)$cov.unbounded
 inits <- lapply(1:reps, function(i) mle$est[1:N]+as.vector(mvtnorm::rmvt(n=1, df=50, sigma=covar)))
 sfInit(parallel=TRUE, cpus=reps)
 sfExportAll()
-fit.hake <-
-  sample_admb(m, iter=iter, init=inits, par.names=par.names, thin=thin,
-              duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup,
+fit.rwm <- sample_admb(m, iter=iter*thin, init=inits, par.names=par.names, thin=thin,
+              duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup*thin,
               dir=d, cores=reps, algorithm='RWM')
-saveRDS(fit.hake, file=paste0("results/long_run_", m, ".RDS")
- launch_shinyadmb(fit.hake)
+saveRDS(fit.rwm, file=paste0("results/long_rwm_", m, ".RDS"))
+fit.nuts <- sample_admb(m, iter=iter, init=inits, par.names=par.names, thin=1,
+              duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup,
+              dir=d, cores=reps, algorithm='NUTS', control=list(adapt_delta=.9))
+saveRDS(fit.nuts, file=paste0("results/long_nuts_", m, ".RDS"))
+
