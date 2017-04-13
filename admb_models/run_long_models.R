a9114bf3cfdbf395eb43138bfc8b6022fc218c1c
colemonnahan <monnahc@uw.edu>
2017-04-13 10:34:08 -0700

    Run long snowcrab model

--- admb_models/run_long_models.R
+++ admb_models/run_long_models.R
@@ -54,10 +54,9 @@ fit.nuts <- sample_admb(m, iter=iter, init=inits, par.names=par.names, thin=1,
               dir=d, cores=reps, algorithm='NUTS', control=list(adapt_delta=.9))
 saveRDS(fit.nuts, file=paste0("results/long_nuts_", m, ".RDS"))
 
-
 sfStop()
 d <- m <- 'tanner'
-thin <- 1000
+thin <- 1
 iter <- 1000
 warmup <- iter/4
 mle <- r4ss::read.admbFit(paste0(d,'/',m))
@@ -67,15 +66,14 @@ par.names <- paste0(1:N, "_", mle$names[1:N])
 covar <- get.admb.cov(d)$cov.bounded
 inits <- lapply(1:reps, function(i) mle$est[1:N]+as.vector(mvtnorm::rmvt(n=1, df=50, sigma=covar)))
 inits <- lapply(1:reps, function(i) mle$est[1:N])
-inits <- NULL
 sfInit(parallel=TRUE, cpus=reps)
 sfExportAll()
 fit.rwm <- sample_admb(m, iter=iter*thin, init=inits, par.names=par.names, thin=thin,
-              duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup*thin,
+             parallel=TRUE, chains=reps, warmup=warmup*thin,
               dir=d, cores=reps, algorithm='RWM')
 saveRDS(fit.rwm, file=paste0("results/long_rwm_", m, ".RDS"))
 fit.nuts <- sample_admb(m, iter=iter, init=inits, par.names=par.names, thin=1,
-              duration=hh*60, parallel=TRUE, chains=reps, warmup=warmup,
+               parallel=TRUE, chains=reps, warmup=warmup,
               dir=d, cores=reps, algorithm='NUTS', control=list(adapt_delta=.9))
 saveRDS(fit.nuts, file=paste0("results/long_nuts_", m, ".RDS"))
 
@@ -105,8 +103,8 @@ saveRDS(fit.nuts, file=paste0("results/long_nuts_", m, ".RDS"))
 
 sfStop()
 d <- 'snowcrab'; m <- '2016sc'
-thin <- 100
-iter <- 500
+thin <- 1000
+iter <- 1000
 warmup <- iter/4
 mle <- r4ss::read.admbFit(paste0(d,'/',m))
 N <- mle$nopar
