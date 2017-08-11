db0222ba313e8a59941e9eaf8f1d8dd5df1750ea
colemonnahan <monnahc@uw.edu>
2017-08-11 10:50:06 -0700

    Update parallel tests -- they are working for NUTS and RWM

--- parallel_tests.R
+++ parallel_tests.R
@@ -16,25 +16,41 @@ compile(file='mvnd.cpp')
 dyn.load(dynlib('mvnd'))
 data <- list(covar=covar, Npar=Npar, x=rep(0, len=Npar))
 mvnd.obj <- MakeADFun(data=data, parameters=list(mu=c(0,0)), DLL='mvnd')
-## setwd('admb')
-## write.table(x=c(Npar, covar), file='mvnd.dat', row.names=FALSE, col.names=FALSE)
+setwd('admb')
+write.table(x=c(Npar, covar), file='mvnd.dat', row.names=FALSE, col.names=FALSE)
 ## system('admb mvnd')
-## system('mvnd')
-## setwd('..')
+system('mvnd -hbf 1')
+setwd('..')
 
-sfInit(parallel=TRUE, cpus=2)
-inits <- lapply(1:2, function(i)  list(mu=c(0,0)))
-admb <- sample_admb(model, iter=2000, dir=dir, parallel=TRUE, chains=2,
-                    cores=3, init=inits, algorithm='RWM')
-
-
-init <- NULL
-init <- function() list(mu=rnorm(2))
 devtools::install("c:/Users/cole/adnuts")
-tmb <- sample_tmb(obj=mvnd.obj, iter=205, init=init, chains=2,
-                  parallel=TRUE, cores=2)
 
+alg <- "RWM"; system('admb/mvnd')
+alg <- "NUTS"; system('admb/mvnd -hbf 1')
+control <- list(metric=covar)
+## Make sure the seeds work across algorithms and parallel and serial.
+inits <- lapply(1:2, function(i)  list(mu=c(0,0)))
+admb.para <- sample_admb('mvnd', iter=2000, dir='admb', parallel=TRUE, chains=2,
+                         cores=3, init=inits, algorithm=alg, seeds=1:2,
+                         control=control)
+admb.serial <- sample_admb('mvnd', iter=2000, dir='admb', parallel=FALSE, chains=2,
+                           cores=3, init=inits, algorithm=alg, seeds=1:2,
+                           control=control)
+init <- function() list(mu=c(0,0))
+tmb.para <- sample_tmb(obj=mvnd.obj, iter=200, init=init, chains=2,
+                       parallel=TRUE, cores=2, seeds=2:3, algorithm=alg,
+                       control=control, lower=c(-1,-1))
+tmb.serial <- sample_tmb(obj=mvnd.obj, iter=200, init=init, chains=2,
+                         parallel=FALSE, cores=2, seeds=2:3, algorithm=alg,
+                         control=control, lower=c(-1,-1))
+## Make sure samples work with same seeds; these should match
+tail(extract_samples(admb.para)[,1,],1)
+tail(extract_samples(admb.serial)[,1,],1)
+tail(extract_samples(tmb.para)[,1,],1)
+tail(extract_samples(tmb.serial)[,1,],1)
 
+### Use this to test the function outside of the package. Useful for
+### debugging
+###
 ## devtools::install("c:/Users/cole/adnuts")
 ## inits <- lapply(1:3, function(i)  list(mu=rnorm(2)))
 ## sfInit(parallel=TRUE, slaveOutfile='out.txt', cpus=2)
