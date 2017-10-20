1dc2c567f191450dda67a5e47b5c1ff128576b7e
colemonnahan <monnahc@uw.edu>
2017-10-20 14:10:31 -0300

    Update iidz simulation script

--- models/iidz/simulation.R
+++ models/iidz/simulation.R
@@ -1,5 +1,5 @@
 ## Generate simulated data
-
+m <- 'iidz'
 adapt.list <- perf.list <- list()
 k <- 1
 for(i in seq_along(Npar.vec)){
@@ -8,20 +8,18 @@ for(i in seq_along(Npar.vec)){
 
   ## Update data, inits and model objects
   data <- list(n=Npar, x=rep(0, Npar))
-  inits <- lapply(1:length(seeds), function(i) list(mu=rnorm(Npar)))
-  obj.stan <- stan(fit=obj.stan, data=data, iter=100,
-                   chains=1, init=list(inits[[1]]), verbose=TRUE)
-  obj.tmb <- MakeADFun(data=data, parameters=inits[[1]], DLL=m)
+  inits <- function(i) list(mu=rnorm(Npar))
+  obj.tmb <- MakeADFun(data=data, parameters=inits(), DLL=m)
   setwd('admb')
   write.table(x=unlist(data), file=paste0(m,'.dat'), row.names=FALSE,
                                           col.names=FALSE )
-  system(paste(m, '-hbf')); setwd('..')
+  system(paste(m, '-maxfn 1')); setwd('..')
   ## Run with updated model size
   temp <- run.chains(obj.stan=obj.stan, obj.tmb=obj.tmb, model=m,
                      data=data, inits=inits, pars=pars, metric='diag', seeds=seeds,
-                     Nout=Nout, Nthin=1, delta=delta, useRWM=TRUE)
-  adapt.list[[k]] <- cbind(cor=j,temp$adapt)
-  perf.list[[k]] <- cbind(cor=j,temp$perf)
+                     Nout=1000, Nthin=1)
+  adapt.list[[k]] <- cbind(temp$adapt)
+  perf.list[[k]] <- cbind(temp$perf)
   ## Save them as we go in case it fails
   perf <-  do.call(rbind, perf.list)
   adapt <- do.call(rbind, adapt.list)
