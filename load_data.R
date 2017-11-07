30852eb626425fb592a5df625f044fe3e4a31ea3
colemonnahan <monnahc@uw.edu>
2017-11-07 10:43:58 -0300

    Rework load_data a bit for new platforms and models

--- load_data.R
+++ load_data.R
@@ -1,4 +1,5 @@
 setwd(main.dir)
+
 ## Loop through and get the maximum correlation of each model for merging
 ## into the main results.
 ## cor.table <- ldply(list.files('models'), function(i) {
@@ -10,30 +11,32 @@ setwd(main.dir)
 ##             })
 empirical <- ldply(list.files('results', pattern='perf_empirical'), function(i)
     read.csv(paste0('results/',i)))
+
+
 ## normalize by maximum run time across delta.target values
 empirical <-
     ddply(empirical, .(platform, model, delta.target), mutate,
-          mean.efficiency=mean(samples.per.time),
-          sd.efficiency=sd(samples.per.time),
-          median.efficiency=quantile(samples.per.time, probs=.5),
-          lwr.efficiency=min(samples.per.time),
-          upr.efficiency=max(samples.per.time))
+          mean.efficiency=mean(efficiency),
+          sd.efficiency=sd(efficiency),
+          median.efficiency=quantile(efficiency, probs=.5),
+          lwr.efficiency=min(efficiency),
+          upr.efficiency=max(efficiency))
 ## empirical <- merge(x=empirical, y=cor.table, by='model')
 empirical.means <-
     ddply(empirical, .(platform, model, Npar), summarize,
-          mean.samples.per.time=mean(samples.per.time))
+          mean.efficiency=mean(efficiency))
 empirical.means.wide <- dcast(subset(empirical.means), model+Npar~platform,
-                              value.var='mean.samples.per.time')
-empirical.means.wide$stan_re <- with(empirical.means.wide, round(stan.nuts/tmb.nuts, 2))
+                              value.var='mean.efficiency')
+## empirical.means.wide$stan_re <- with(empirical.means.wide, round(stan/tmb, 2))
 simulated <- ldply(list.files('results', pattern='perf_simulated'), function(i)
     read.csv(paste0('results/',i)))
 simulated <-
-    ddply(simulated, .(platform, model, delta.target, Npar, cor), mutate,
-          mean.efficiency=mean(samples.per.time),
-          sd.efficiency=sd(samples.per.time),
-          median.efficiency=quantile(samples.per.time, probs=.5),
-          lwr.efficiency=min(samples.per.time),
-          upr.efficiency=max(samples.per.time))
+    ddply(simulated, .(platform, model, delta.target, Npar), mutate,
+          mean.efficiency=mean(efficiency),
+          sd.efficiency=sd(efficiency),
+          median.efficiency=quantile(efficiency, probs=.5),
+          lwr.efficiency=min(efficiency),
+          upr.efficiency=max(efficiency))
 ## ## Select Stan modles with default delta.target level
 ## growth <-
 ##     subset(simulated, model %in%
@@ -44,12 +47,12 @@ simulated <-
 ## growth$normal <- 'normal'
 ## growth$normal[growth$model %in% c('growth_t', 'growth_nct')] <- 'student-t'
 ## growth.means <- ddply(growth, .(platform, model, Npar, normal, centered), summarize,
-##                       mean.samples.per.time=round(mean(samples.per.time),2))
-## growth.means.wide <- dcast(subset(growth.means), model+Npar+centered+normal~platform, value.var='mean.samples.per.time')
+##                       mean.efficiency=round(mean(efficiency),2))
+## growth.means.wide <- dcast(subset(growth.means), model+Npar+centered+normal~platform, value.var='mean.efficiency')
 ## growth.means.wide$stan_re <- with(growth.means.wide, round(stan.nuts/jags, 2))
 mvn <- subset(simulated, model %in% c('mvnd', 'mvnc'))
 mvn.means <- ddply(mvn, .(platform, model, Npar, cor), summarize,
-                      mean.samples.per.time=round(mean(samples.per.time),2))
+                      mean.efficiency=round(mean(efficiency),2))
 
 ## summarize adaptation info
 adapt_empirical<- ldply(list.files('results', pattern='adapt_empirical'), function(i)
@@ -66,6 +69,8 @@ write.table(simulated, file='results/simulated.csv', sep=',',
 ##             row.names=FALSE, col.names=TRUE)
 ## write.table(file='results/table_perf.csv', x=empirical.means.wide, sep=',',
 ##             row.names=FALSE, col.names=TRUE)
-print(subset(empirical, platform=='tmb.nuts' & seed ==seeds[1],
+
+## Check that everything worked properly among platforms and models
+print(subset(empirical, platform=='tmb' & seed ==seeds[1],
              select=c(model, Nsims)))
 print(ddply(empirical, .(model, platform), summarize, reps=length(seeds)))
