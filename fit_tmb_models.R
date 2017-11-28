978ce547c18b28ed26e38de1c3e13be00543ef58
colemonnahan <monnahc@uw.edu>
2017-11-28 09:10:32 -0300

    Add tmbstan calls to tmb file

--- fit_tmb_models.R
+++ fit_tmb_models.R
@@ -1,5 +1,8 @@
 ## Script to do MLE fits for the example models
 
+## Working directory needs to be set to the folder of this file before
+## proceeding.
+
 source("startup.R")
 
 ## swallows model
@@ -7,21 +10,23 @@ data <- swallows_setup()$data
 inits <- swallows_setup()$inits
 compile('models/swallows/swallows.cpp')
 dyn.load('models/swallows/swallows')
-obj.tmb <-
+obj <-
   MakeADFun(data=data, parameters=inits(),
             random=c('fameffphi_raw', 'fameffp_raw', 'yeareffphi_raw'),
             DLL='swallows')
-fit.swallows <- with(obj.tmb, nlminb(par, fn, gr))
+fit.swallows <- with(obj, nlminb(par, fn, gr))
+mcmc.swallows <- tmbstan(obj, iter=2000, chains=3)
 
 ## wildf model
 data <- wildf_setup()$data
 inits <- wildf_setup()$inits
 compile('models/wildf/wildf.cpp')
 dyn.load('models/wildf/wildf')
-obj.tmb <-
+obj <-
   MakeADFun(data=data, parameters=inits(),
             random=c('yearInterceptEffect_raw',
                      'plantInterceptEffect_raw',
                      'plantSlopeEffect_raw'),
             DLL='wildf')
-fit.wildf <- with(obj.tmb, nlminb(par, fn, gr))
+fit.wildf <- with(obj, nlminb(par, fn, gr))
+mcmc.wildf <- tmbstan(obj, iter=2000, chains=3)
