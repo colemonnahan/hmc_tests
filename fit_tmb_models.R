3be35a92baeddbbef03622c0093e66f561e3fcb3
colemonnahan <monnahc@uw.edu>
2017-11-28 09:23:17 -0300

    Add some comments to tmb file

--- fit_tmb_models.R
+++ fit_tmb_models.R
@@ -3,6 +3,12 @@
 ## Working directory needs to be set to the folder of this file before
 ## proceeding.
 
+## Note: these models include priors, and use exponentiation in the
+## template with a Jacobian adjustment instead of external bounds of
+## (0,Inf) for hypervariance parameters. Thus, the SD parameters are really
+## in log space. See template for more details. Should we turn the priors
+## off for MLE?
+
 source("startup.R")
 
 ## swallows model
