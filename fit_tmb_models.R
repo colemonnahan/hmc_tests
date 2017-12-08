32c36259f93314425c798a3ff8079a93783f4dc3
colemonnahan <monnahc@uw.edu>
2017-12-08 17:30:31 -0300

    Add MCMC vs MLE comparison for wildf

--- fit_tmb_models.R
+++ fit_tmb_models.R
@@ -24,7 +24,7 @@ fit.swallows <- with(obj, nlminb(par, fn, gr))
 mcmc.swallows <- tmbstan(obj, iter=2000, chains=3)
 
 ## wildf model
-data <- wildf_setup()$data
+data <- wildf_se tup()$data
 inits <- wildf_setup()$inits
 compile('models/wildf/wildf.cpp')
 dyn.load('models/wildf/wildf')
@@ -35,4 +35,20 @@ obj <-
                      'plantSlopeEffect_raw'),
             DLL='wildf')
 fit.wildf <- with(obj, nlminb(par, fn, gr))
-mcmc.wildf <- tmbstan(obj, iter=2000, chains=3)
+mcmc.wildf <- tmbstan(obj, iter=2000, chains=1)
+
+## Quickly rerun to plot MCMC vs MLE for fixed effects
+mcmc.wildf2 <- sample_tmb(obj, iter=5000, warmup=1000, init=inits, chains=3, cores=3,
+                          parallel=TRUE, path='models/wildf', control=list(adapt_delta=.59)
+fit.wildf <- with(obj, nlminb(par, fn, gr))
+## Have to manually modify the list so that pairs_admb can use it. It's not
+## designed for tmb output.
+sd <- sdreport(obj)
+se <- sqrt(diag(sd$cov.fixed))
+cor <- sd$cov.fixed / se %o% se
+mle <- list(par.names=dimnames(mcmc.wildf2$samples)[[3]][1:9],
+            est=fit.wildf$par, se=se, cor=cor)
+mcmc.wildf2$mle <- mle
+png('pairs_wildf.png', width=6, height=4, units='in', res=500)
+pairs_admb(mcmc.wildf2, pars=mcmc.wildf2$mle$par.names)
+dev.off()
