70440800eb787663aa24e8f4c97e8759d67716f0
colemonnahan <monnahc@uw.edu>
2017-12-13 11:43:04 -0800

    Rework LA checks and save result file

--- fit_tmb_models.R
+++ fit_tmb_models.R
@@ -61,8 +61,11 @@ obj <-
                      'plantSlopeEffect_raw'),
             DLL='wildf')
 fit.wildf <- with(obj, nlminb(par, fn, gr))
-mcmc.wildf <- tmbstan(obj, iter=2000, chains=3)
-mcmc.wildf.la <- tmbstan(obj, iter=2000, chains=3, laplace=TRUE)
+## Run long, thinned chains to ensure mixing is relatively close
+th <- 10 # thin rate
+wm <- 1000 # warmup
+mcmc.wildf <- tmbstan(obj, iter=2000*th+wm, warmup=wm, thin=th, chains=3)
+mcmc.wildf.la <- tmbstan(obj, iter=2000*thin+wm, warmup=wm, thin=th, chains=3, laplace=TRUE)
 
 temp <- extract(mcmc.wildf, permuted=FALSE)
 x1 <- do.call(rbind, lapply(1:3,function(i)  temp[,i,]))
@@ -71,6 +74,8 @@ x2 <- do.call(rbind, lapply(1:3,function(i)  temp[,i,]))
 pars <- dimnames(x2)[[2]][1:9]
 post <- data.frame(rbind(x1[,pars], x2[,pars]))
 model <- as.factor(rep(c("normal", "LA"), each=3000))
+saveRDS(cbind(model,post), file='results/post.wildf.RDS')
+
 png("pairs_wildf_LA.png", width=7, height=5, units='in', res=200)
 pairs(post, col=model, pch='.')
 dev.off()
